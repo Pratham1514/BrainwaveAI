5f9a7090ea43296b14314bc02d548a367cfb7cab
Pratham <prathameshgite366@gmail.com>
2024-07-02 21:22:07 +0530

    Landing page complete

---
+++ src/components/CompanyLogo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { companyLogos } from "../constants";
+
+const CompanyLogo = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beatiful content at
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            key={index}
+          >
+            <img src={logo} alt={logo} width={134} height={28} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CompanyLogo;
